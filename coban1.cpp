d27cb7680001a2fa1a7e89433ee257b4c535b0f9
FanFan3107 <139844696+FanFan3107@users.noreply.github.com>
2023-07-18 16:16:08 +0700

    Add files via upload

--- coban1.cpp
+++ coban1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "********************\n";
-    cout << "* THAO CHUONG BANG *\n";
-    cout << "*     NGON NGU C   *\n";
-    cout << "********************";
+    int h=15,w=30;
+    for( int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            if (i ==  h/2 && j== w / 2-2)
+            {
+                cout<<"THAO CHUONG BANG NGON NGU C";
+                j+=5;
+            }
+            else if ( i ==0 || i == h-1 ||j ==0 ||j==w-1)
+            {
+                cout<<"*";
+            }
+            else
+                cout<<" ";
+        }
+        cout<<"\n";
+    }
 }
